b25bdc9737304eaa0cf729b4a5bf2e0afec453fd
Donald Maness <ddmaness@gmail.com>
2018-11-15 11:33:49 -0500

    Add images

---
+++ dist/index.js
@@ -0,0 +1,112 @@
+Vue.component('landing-footer', {
+  template: `<Footer>
+                <div>
+                    <ul>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Terms of Use</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                    </ul>
+                    <p>Vue Landing Page [copywrite symbol] 2018</p>
+                </div>
+                <div>
+                    <ul>
+                        <li href="#">[soc media]</li>
+                        <li href="#">[soc media]</li>
+                        <li href="#">[soc media]</li>
+                    </ul>
+                </div>
+                
+                </Footer>`
+});
+Vue.component('call-to-action', {
+  template: `<Section class="call-to-action">
+                    <h2>Why wait? Get started today!</h2>
+                    <slot></slot>
+                </Section>`
+});
+Vue.component('image-text-sbs', {
+  props: ['heading', 'text', 'src', 'alt'],
+  template: `<Section class='image-text-sbs'>
+                    <div>
+                        <h2>{{ heading }}</h2>
+                        <p>{{ text }}</p>
+                    </div>
+                    <div data-src="src"></div>
+                </Section>`
+});
+Vue.component('list', {
+  template: `<Section class="list">
+                    <slot></slot>
+                </Section>`
+});
+Vue.component('list-item', {
+  props: ['heading', 'text', 'src', 'alt'],
+  template: `<div>
+                    <img :src="src">
+                    <h3>{{ heading }}</h3>
+                    <p>{{ text }}</p>
+                </div>`
+});
+Vue.component('page-header', {
+  template: `<Header>
+                    <h3 class="header-text">Vue Landing Page</h3>
+                    <button class="header-btn">Sign In</button>
+                </Header>`
+});
+Vue.component('hero', {
+  template: `<Section class="hero">
+                    <h1 class="hero-text">A landing page created using the Vue framework</h1>
+                    <slot></slot>
+                </Section>`
+});
+Vue.component('sign-up-form', {
+  template: `<form> 
+                    <input type="email" placeholder="Please enter your email"/>
+                    <button type="submit">Sign up!</button>
+                </form>`
+});
+var app = new Vue({
+  el: '#app',
+  data: {
+    benefitItems: [{
+      src: 'images/cat.jpg',
+      heading: 'Cat',
+      text: 'a cat'
+    }, {
+      src: 'images/cat.jpg',
+      heading: 'Cat',
+      text: 'a cat'
+    }, {
+      src: 'images/cat.jpg',
+      heading: 'Cat',
+      text: 'a cat'
+    }],
+    imageTextCombos: [{
+      src: 'images/dog.jpg',
+      heading: 'Dog',
+      text: 'a dog'
+    }, {
+      src: 'images/dog.jpg',
+      heading: 'Dog',
+      text: 'a dog'
+    }, {
+      src: 'images/dog.jpg',
+      heading: 'Dog',
+      text: 'a dog'
+    }],
+    testimonials: [{
+      src: 'images/dog.jpg',
+      heading: 'Dog',
+      text: 'a dog'
+    }, {
+      src: 'images/dog.jpg',
+      heading: 'Dog',
+      text: 'a dog'
+    }, {
+      src: 'images/dog.jpg',
+      heading: 'Dog',
+      text: 'a dog'
+    }]
+  }
+});
--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,65 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
+var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var pngquant = require('imagemin-pngquant');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('default', function(){
-    gulp.watch('sass/styles.scss', gulp.series('styles'));
-    gulp.watch('./index.js').on('change', browserSync.reload)
-    gulp.watch('./index.html').on('change', browserSync.reload)
-    browserSync.init({
-        server: './'
-    });
-})
+	gulp.watch('sass/styles.scss', gulp.series('styles'));
+	gulp.watch('./index.js').on('change', browserSync.reload);
+	gulp.watch('./index.html', gulp.series('copy-html'));
+	gulp.watch('./dist/index.html').on('change', browserSync.reload);
+	browserSync.init({
+		server: './dist'
+	});
+});
+
+gulp.task('copy-html', function(done){
+	gulp.src('./index.html')
+		.pipe(gulp.dest('./dist'));
+	done();
+});
+
+gulp.task('copy-images', function(){
+	return gulp.src('images/*')
+		.pipe(imagemin({
+			progressive: true,
+			use: [pngquant()]
+		}))
+		.pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('scripts', function(done){
+	gulp.src('./index.js')
+		.pipe(babel())
+	//skipping concatenation because only one file is present
+		.pipe(gulp.dest('dist/'));
+	done();
+});
+
+gulp.task('scripts-dist', function(done){
+	gulp.src('./index.js')
+		.pipe(sourcemaps.init())
+		.pipe(babel())
+	//skipping concatenation because only one file is present
+		.pipe(uglify())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('dist/'));
+	done();
+});
 
 gulp.task('styles', function(done){
-    gulp.src('sass/styles.scss')
-        .pipe(sass()).on('error', sass.logError)
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-        }))
-        .pipe(gulp.dest('./css'))
-        .pipe(browserSync.stream());
-    done();
-})
+	gulp.src('sass/styles.scss')
+		.pipe(sass()).on('error', sass.logError)
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+		}))
+		.pipe(gulp.dest('dist/css'))
+		.pipe(browserSync.stream());
+	done();
+});
+
+gulp.task('dist', gulp.parallel('copy-html', 'copy-images', 'styles', 'scripts-dist'));
